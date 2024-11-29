b7a4d2cbe88833d45768fd602a2fb376a9eb2e8a
Viviane de Lima <38511974+zennom@users.noreply.github.com>
2024-11-29 11:52:35 -0300

    Update pessoaController.ts

--- src/controllers/pessoaController.ts
+++ src/controllers/pessoaController.ts
@@ -1,12 +1,19 @@
 import {Request, Response} from 'express'
 import {Pessoa} from '../models/Pessoa'
-
+import {Curso} from '../models/Curso'
 
 export const index = async (req: Request, res: Response) => {
-    let pessoa = await Pessoa.findAll()
+  
+  const pessoasComCursos = await Pessoa.findAll({
+    include: {
+      model: Curso,
+      as: 'curso', // Nome do alias
+      required: false, // Caso nem todas as pessoas tenham curso preferido
+    },
+  })
 
-    // Renderizando o template com os dados de 'pessoa', agora com o curso incluído
-    res.render('pages/pessoa', {
-      pessoa
-    });
-  }
+  res.render('pages/pessoa', {
+    pessoasComCursos
+  });
+}
+
